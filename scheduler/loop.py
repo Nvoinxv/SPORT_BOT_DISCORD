40b879353e67b8e1d2bb7ba089ff320a7d40e83e
Nvoinxv <sukinvoin67@gmail.com>
2026-07-30 00:52:14 +0800

    penambahan scheduler dan trigger ketika deploy

--- scheduler/loop.py
+++ scheduler/loop.py
@@ -1,6 +1,11 @@
+import datetime
 from discord.ext import tasks
 from services.notifier import NotifierService
 from utils.logger import logger
+
+# WITA (Waktu Indonesia Tengah) adalah UTC+8
+WITA_TZ = datetime.timezone(datetime.timedelta(hours=8))
+SCHEDULE_TIME = datetime.time(hour=10, minute=0, tzinfo=WITA_TZ)
 
 class ReminderLoop:
     def __init__(self, bot):
@@ -8,7 +13,7 @@ class ReminderLoop:
         self.notifier = NotifierService(bot)
         self.check_matches.start()
 
-    @tasks.loop(hours=24)
+    @tasks.loop(time=SCHEDULE_TIME)
     async def check_matches(self):
         logger.info("Scheduler Triggered: Checking for matches...")
         await self.notifier.check_and_notify()
@@ -17,3 +22,6 @@ class ReminderLoop:
     async def before_check(self):
         await self.bot.wait_until_ready()
         logger.info("Scheduler is ready and waiting for loops.")
+        # Memicu output langsung saat bot pertama kali jalan (deploy)
+        logger.info("Triggering initial check immediately on deploy...")
+        await self.notifier.check_and_notify()
